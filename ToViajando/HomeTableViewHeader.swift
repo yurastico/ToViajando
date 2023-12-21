d3fb11f6b41863a155189d6b52039893fc7b1cfd
Yuri Cunha <yuricunha@MacBook-Pro-de-Yuri.local>
2023-12-21 06:35:36 -0300

    creting headerView

---
+++ ToViajando/HomeTableViewHeader.swift
@@ -0,0 +1,20 @@
+//
+//  HomeTableViewHeader.swift
+//  ToViajando
+//
+//  Created by Yuri Cunha on 21/12/23.
+//
+
+import UIKit
+
+class HomeTableViewHeader: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- ToViajando/ViewController.swift
+++ ToViajando/ViewController.swift
@@ -12,13 +12,16 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tripsTableView.delegate = self
+        tripsTableView.dataSource = self
+        view.backgroundColor = UIColor(red: 30/255, green: 59.0/255, blue: 119.0/255, alpha: 1)
         // Do any additional setup after loading the view.
     }
 
 
 }
 
-extension ViewController: UITableViewDataSource {
+extension ViewController: UITableViewDataSource,UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
     }
@@ -29,5 +32,13 @@ extension ViewController: UITableViewDataSource {
         return cell
     }
     
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = Bundle.main.loadNibNamed("HomeTableViewHeader", owner: self)
+        
+        return headerView?.first as? HomeTableViewHeader
+    }
     
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        300
+    }
 }
