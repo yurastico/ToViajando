159a648722e96cfa7aa610adfcca169edfd7a96a
yurastico <cunha.ysc@gmail.com>
2023-12-27 03:16:48 -0300

    config cell

--- ToViajando/View/Cells/TripTableViewCell.swift
+++ ToViajando/View/Cells/TripTableViewCell.swift
@@ -23,4 +23,22 @@ class TripTableViewCell: UITableViewCell {
     @IBOutlet weak var priceDiscontLabl: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var cancellationStatusLabel: UILabel!
+    
+    
+    func configCell(_ trip: Viagem?) {
+        tripImage.image = UIImage(named: trip?.asset ?? "")
+        titleTripLabel.text = trip?.titulo
+        subtitleTripLabel.text = trip?.subtitulo
+        priceLabel.text = "R$ \(trip?.preco ?? 0)"
+        let atributtedString = NSMutableAttributedString(string: "R$ \(trip?.precoSemDesconto ?? 0)")
+        atributtedString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: 1, range: NSMakeRange(0,atributtedString.length))
+        priceDiscontLabl.attributedText = atributtedString
+        
+        if let numberOfDays = trip?.diaria, let numberOfGuests = trip?.hospedes {
+            let days = numberOfDays == 1 ? "Diaria" : "Diarias"
+            let guests = numberOfGuests == 1 ? "Pessos" : "Pessoas"
+            diaryTripLabel.text = "\(numberOfDays) \(days) - \(numberOfGuests) \(guests)"
+        }
+        
+    }
 }
